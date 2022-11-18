d4e5fcf5477a7f610ea0eea2482adde2ac0946fb
atulinfinite <atul.kumar2@infinite.com>
2022-11-18 17:34:10 +0530

    state Changes

--- src/about/About.js
+++ src/about/About.js
@@ -1,15 +1,51 @@
-import React from 'react'
-import Header from "../common/header/Header"
-import Footer from "../common/footer/Footer"
+import React, { useState } from "react";
+import Header from "../common/header/Header";
+import Footer from "../common/footer/Footer";
+import "./About.scss";
+
+const tabData = [
+  { id: 0, name: "event" },
+  { id: 2, name: "temperature" },
+  { id: 3, name: "humidity" }
+];
+const CommonTabs = (props) => {
+  
+  const { active, setActive } = props;
+  return (
+    <div className="tabsContainer">
+          <ul className="TabsWrapper">
+            {
+              tabData.map((item,i)=>{
+                return(
+                  <li className={ active===item.id? "item active" :'item'} onClick={()=>setActive(item?.id)}> {item.name} </li>
+                )
+              })
+            }
+            {/* <li className='item' > tab1 </li> */}
+          </ul>
+    </div>
+  );
+};
 
 const About = () => {
+  const [active, setActive] = useState(0);
   return (
-    <div>
-      <Header/>
-            <h1>About</h1>
-        <Footer/>
-    </div>
-  )
-}
+    <div className="aboutMainWrapper">
+      <Header />
 
-export default About
+      <CommonTabs active={active} setActive={setActive}/>
+      {
+        active==0 && (<p>tab1</p>) 
+      }
+      {
+        active==2 && (<p>temperature</p>) 
+      }
+      {
+        active==3 && (<p>humidity</p>) 
+      }
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
--- src/addProduct/AddProduct.js
+++ src/addProduct/AddProduct.js
@@ -1,82 +1,142 @@
-import React, { useState } from 'react'
-import Header from '../common/header/Header'
-import Footer from "../common/footer/Footer"
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import Header from "../common/header/Header";
+import Footer from "../common/footer/Footer";
+import { useNavigate } from "react-router-dom";
 
+// const InputField =({name,placeholder})=>{
+//   const [value , setValue] = useState('')
+//   return (
+//     <input
+//     name={name}
+//     value={value}
+//     placeholder={placeholder}
+//     onClick={(e)=>setValue(e.target.value)} />
+//   )
+// }
 const AddProduct = () => {
-  const [name, setName] = useState('')
-  const [price , setPrice] = useState("")
-  const [category , setCategory] = useState("")
-  const [brand , setBrand] = useState("")
-  const navigate= useNavigate()
-  const [error, setError] = useState(false)
-  let userId = JSON.parse(localStorage.getItem("user"))?._id
+  const [data, setData] = useState({
+    name: "",
+    price: "",
+    brand: "",
+    category: "",
+  });
+console.log(data)
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [brand, setBrand] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  let userId = JSON.parse(localStorage.getItem("user"))?._id;
 
-
-  const collectFormData =async(e)=>{
+  const collectFormData = async (e) => {
     e.preventDefault();
-    console.log(name,price,category,brand)
-    
-    if(!name || !price || !category || !brand){
-      setError(true)
-      console.log(error)
-      return false
+    console.log(name, price, category, brand);
+
+    if (!name || !price || !category || !brand) {
+      setError(true);
+      console.log(error);
+      return false;
     }
     let result = await fetch("http://localhost:5000/addProduct", {
       method: "post",
-      body: JSON.stringify({ name,price,category,brand,userId }),
+      body: JSON.stringify({ name, price, category, brand, userId }),
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-    })
-    result = await result.json()
-    console.log("result", result)
+    });
+    result = await result.json();
+    console.log("result", result);
 
     if (result) {
-      navigate("/")
+      navigate("/");
     }
 
-
-    setBrand("")
-    setCategory("")
-    setName("")
-    setPrice("")
-    
-
-}
+    setBrand("");
+    setCategory("");
+    setName("");
+    setPrice("");
+  };
   return (
     <div>
-      <Header/>
-      <div className='addProductContainer'>
-      <div className='mainContainer' style={{background:"none", color:"black"}}>
-        <header className='loginHead'>
-          <p> Add Product</p>
-        </header>
-        <form className='addProductFrom' autoComplete="off">
-          <div>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Product Name" />
-          { error && !name && <span>Enter Product Name *</span>}
-          </div>
-          <div>
-          <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} placeholder="Enter Product Price"/>
-          {error && !price && <span>Enter Product Price *</span>}
-          </div>
-          <div>
-          <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Enter Product Category" />
-          {error && !category && <span>Enter Product Category *</span>}
-          </div>
-          <div>
-          <input type="text" value={brand} onChange={(e) => setBrand(e.target.value)} placeholder="Enter Prduct brand"/>
-          {error && !brand && <span>Enter Product Brand *</span>}
-          </div>
-          <button onClick={collectFormData} className='loginBtn'>Add Product</button>
-
-        </form>
+      <Header />
+      <div className="addProductContainer">
+        <div
+          className="mainContainer"
+          style={{ background: "none", color: "black" }}
+        >
+          <header className="loginHead">
+            <p> Add Product</p>
+          </header>
+          <form className="addProductFrom" autoComplete="off">
+            <div>
+              <input
+                type="text"
+                value={data?.name}
+                onChange={(e) => {
+                  setData({
+                    ...data,
+                    name: e.target.value,
+                  });
+                }}
+                placeholder="Enter Product Name"
+              />
+              {/* <InputField 
+            name="name"
+            placeholder='Enter Product Name' /> */}
+              {error && !name && <span>Enter Product Name *</span>}
+            </div>
+            <div>
+              <input
+                type="text"
+                value={data?.price}
+                onChange={(e) =>
+                  setData({
+                    ...data,
+                    price: e.target.value,
+                  })
+                }
+                placeholder="Enter Product Price"
+              />
+              {error && !price && <span>Enter Product Price *</span>}
+            </div>
+            <div>
+              <input
+                type="text"
+                value={data?.category}
+                onChange={(e) =>
+                  setData({
+                    ...data,
+                    category: e.target.value,
+                  })
+                }
+                placeholder="Enter Product Category"
+              />
+              {error && !category && <span>Enter Product Category *</span>}
+            </div>
+            <div>
+              <input
+                type="text"
+                value={data?.brand}
+                onChange={(e) =>
+                  setData({
+                    ...data,
+                    brand: e.target.value,
+                  })
+                }
+                placeholder="Enter Prduct brand"
+              />
+              {error && !brand && <span>Enter Product Brand *</span>}
+            </div>
+            <button onClick={collectFormData} className="loginBtn">
+              Add Product
+            </button>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
-        <Footer/>
-    </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
